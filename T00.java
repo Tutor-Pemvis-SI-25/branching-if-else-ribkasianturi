87cd853fee220630d64c78045971aaa63ca3eb7e
ribkasianturi <ribkasianturi32@gmail.com>
2025-10-04 08:28:56 +0700

    PAIR 22

--- T00.java
+++ T00.java
@@ -8,34 +8,34 @@ public class T00 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int bil1, bil2, hasil;
+        int bilangan1, bilangan2, hasil;
 
-        bil1 = Integer.parseInt(input.nextLine());
-        bil2 = Integer.parseInt(input.nextLine());
-        if (bil1 % 2 == 0) {
+        bilangan1 = Integer.parseInt(input.nextLine());
+        bilangan2 = Integer.parseInt(input.nextLine());
+        if (bilangan1 % 2 == 0) {
             System.out.println("bilangan pertama genap");
         } else {
             System.out.println("bilangan pertama ganjil");
         }
-        if (bil2 % 2 == 0) {
+        if (bilangan2 % 2 == 0) {
             System.out.println("bilangan kedua genap");
         } else {
             System.out.println("bilangan kedua ganjil");
         }
-        if (bil1 > bil2) {
+        if (bilangan1 > bilangan2) {
             System.out.println("bilangan pertama  lebih besar");
         } else {
-            if (bil2 > bil1) {
+            if (bilangan2 > bilangan1) {
                 System.out.println("bilangan kedua lebih besar");
             } else {
                 System.out.println("keduanya sama besar");
             }
         }
-        if (bil1 % 2 == 0 && bil2 % 2 == 0) {
-            System.out.println("hasil penjumlahan=" + bil1 * bil2);
+        if (bilangan1 % 2 == 0 && bilangan2 % 2 == 0) {
+            System.out.println("hasil penjumlahan=" + bilangan1 * bilangan2);
         } else {
-            if (bil1 % 2 != 0 && bil2 % 2 != 0) {
-                System.out.println("hasil perkalian=" + bil1 * bil2);
+            if (bilangan1 % 2 != 0 && bilangan2 % 2 != 0) {
+                System.out.println("hasil perkalian=" + bilangan1 * bilangan2);
             } else {
                 System.out.println("berbeda jenis");
             }
