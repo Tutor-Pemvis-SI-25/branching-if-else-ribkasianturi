52a2bac1caf5bc55387191324d56130d6a4cf1f4
ribkasianturi <ribkasianturi32@gmail.com>
2025-10-04 13:41:02 +0700

    pair 22

--- T00.java
+++ T00.java
@@ -1,4 +1,4 @@
-// 12S25018 - Ribka Sianturi
+//12S25018 - Ribka Sianturi
 
 import java.util.*;
 import java.lang.Math;
@@ -7,40 +7,40 @@ public class T00 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int bilangan1, bilangan2, tambah, kali;
+        int a, b, tambah, kali;
 
-        bilangan1 = Integer.parseInt(input.nextLine());
-        bilangan2 = Integer.parseInt(input.nextLine());
-        if (bilangan1 % 2 == 0) {
+        a = Integer.parseInt(input.nextLine());
+        b = Integer.parseInt(input.nextLine());
+        if (a % 2 == 0) {
             System.out.println("Bilangan pertama: Genap");
         } else {
             System.out.println("Bilangan pertama: Ganjil");
         }
-        if (bilangan2 % 2 == 0) {
+        if (b % 2 == 0) {
             System.out.println("Bilangan kedua: Genap");
         } else {
             System.out.println("Bilangan kedua: Ganjil");
         }
-        if (bilangan1 > bilangan2) {
+        if (a > b) {
             System.out.println("Bilangan pertama lebih besar");
         } else {
-            if (bilangan1 < bilangan2) {
+            if (a < b) {
                 System.out.println("Bilangan kedua lebih besar");
             } else {
                 System.out.println("Kedua bilangan sama besar");
             }
         }
-        if (bilangan1 % 2 == 0) {
-            if (bilangan2 % 2 == 0) {
-                tambah = bilangan1 + bilangan2;
-                System.out.println("hasil penjumlahan: " + tambah);
+        if (a % 2 == 0) {
+            if (b % 2 == 0) {
+                tambah = a + b;
+                System.out.println("Hasil penjumlahan: " + tambah);
             } else {
                 System.out.println("Berbeda jenis");
             }
         } else {
-            if (bilangan2 % 2 != 0) {
-                kali = bilangan1 * bilangan2;
-                System.out.println("hasil perkalian:" + kali);
+            if (b % 2 != 0) {
+                kali = a * b;
+                System.out.println("Hasil perkalian: " + kali);
             } else {
                 System.out.println("Berbeda jenis");
             }
